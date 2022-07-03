c3fa639cb73695be45ad0eaa111f02d6a4ee22a2
Leonardo Nunes <leonardoportelanunes@gmail.com>
2022-07-03 17:33:39 -0400

    done

---
+++ script.js
@@ -0,0 +1,59 @@
+let comedy = [
+    {
+        name:"Central Intelligence",
+        director:'Rawson Marshall Thurber',
+        year:2016
+    },
+    {
+        name:'This is the end',
+        director:'Evan Goldberg & Seth Rogen',
+        year:2013
+    },
+    {
+        name:'Scouts guide to the zombie apocalypse',
+        director:'Christopher B. Landon',
+        year:2015
+    }
+]
+
+let action=[
+    {
+        name:'007-No time to die',
+        director:'Cary Fukunaga',
+        year:2021
+    },
+    {
+        name:'Danny The Dog',
+        director:'Louis Leterrier',
+        year:2005
+    },
+    {
+        name:'Kingsman:Secret Service',
+        director:'Matthew Vaughn',
+        year:2014
+    }
+]
+
+let fantasy=[
+    {
+        name:'Harry Potter and the Deathly Hallows – Part 1',
+        director:'David Yates',
+        year:2010
+    },
+    {
+        name:'Percy Jackson & the Olympians:The Lightning Thief',
+        director:'Chris Columbus',
+        year:2010
+    },
+    {
+        name:'Arthur and the Invisibles',
+        director:'Luc Besson',
+        year:2006
+    }
+]
+
+if(fantasy[0].name === 'Harry Potter and the Secret Chamber'){
+    console.log('My favorite movie is Harry Potter and the Secret Chamber')
+}else{
+    console.log('Not even 5 stars...')
+}
